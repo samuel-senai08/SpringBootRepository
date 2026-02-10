cab133056af4b8f699b0706829f6045a6bbb2c0e
Samuel <bonifaciosamuel2107@gmail.com>
2026-02-10 15:13:43 -0300

    Update VacinacaoRepository.java

--- VacinacaoRepository.java
+++ VacinacaoRepository.java
@@ -1,5 +1,8 @@
 package com.petshop.demo.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.petshop.demo.Entity.Vacinacao;
 
-public interface VacinacaoRepository {
+public interface VacinacaoRepository extends JpaRepository<Vacinacao, Long>{
 
 }
+
