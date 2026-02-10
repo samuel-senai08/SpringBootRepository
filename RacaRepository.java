283a8fa5cf0f2afab2b26e986b5a68fac211007a
Samuel <bonifaciosamuel2107@gmail.com>
2026-02-10 15:12:41 -0300

    Update RacaRepository.java

--- RacaRepository.java
+++ RacaRepository.java
@@ -1,5 +1,8 @@
 package com.petshop.demo.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.petshop.demo.Entity.Raca;
 
-public interface RacaRepository {
+public interface RacaRepository extends JpaRepository<Raca, Long> {
 
 }
+
